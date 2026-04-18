5ac9caaee9f9b1b8f528e850de6ccf78a2cb0caa
Shivangi Sritharan <ssrith@proton.me>
2026-04-18 10:57:46 +0530

    v0.3.8 - testing render deploy

--- app/call/forms.py
+++ app/call/forms.py
@@ -1,3 +1,13 @@
+'''
+app/call/forms.py
+Created by Shivangi Sritharan
+Last modified: 18/04/2026
+
+This file contains the server-side code for 
+forms related to the call room. The forms are 
+implemented with Flask-WTForms.
+'''
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -6,6 +16,7 @@ class JoinForm(FlaskForm):
     user_name = StringField(
         "Your Name", 
         validators=[DataRequired()],
+        # render_kw tells the application how to render the form with CSS
         render_kw={"class": "input", "placeholder": "Your Name"}
     )
     room_code = StringField(
@@ -19,7 +30,8 @@ class JoinForm(FlaskForm):
         render_kw={"class": "btn btn-primary btn-block"}
     )
 
-
+# this is a simple submit-only form because current logic requires
+# users to be signed in to create a room.
 class CreateRoomForm(FlaskForm):
     submit = SubmitField(
         "Create New Session",
