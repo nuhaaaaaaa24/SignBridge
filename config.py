49c5426777b54064305946c34047b412c7211fe4
Shivangi Sritharan <ssrith@proton.me>
2026-05-08 15:40:47 +0530

    modified config.py so ppl stop breathing down my neck lmao

--- config.py
+++ config.py
@@ -16,32 +16,21 @@ load_dotenv(os.path.join(basedir, '.env')) # set variables before class is const
 class Config:
 
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=30) # session lifetime of 30 minutes. This means that if a user is inactive for 30 minutes, they will be logged out. This is a security measure to prevent unauthorized access to accounts.
-    # secret key for token generation
-    # todo: add a real secret key lol
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') # secret key for token generation
 
-    # the flask-SQLAlchemy extension takes the location of the application's 
-    # database from the SQLALCHEMY_DATABASE_URI configuration variable
-    # if the url isn't defined in DATABASE_URL it will fall back to
-    # app.db found locally.
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://signbridge:thesbdatabase123@localhost:5432/signbridgedb'
-
-    uri = os.environ.get('DATABASE_URL') # or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    if not uri:
-        raise RuntimeError("DATABASE_URL is not set. Application will not start.")
-
+    uri = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql+psycopg2://", 1)
 
-    SQLALCHEMY_DATABASE_URI = uri
+    SQLALCHEMY_DATABASE_URI = uri 
 
     # the flask-limiter extension stores data in this
     # because limiter uses the limit library this is kept separate from sqlalchemy
     RATELIMIT_STORAGE_URI = os.environ.get("RATELIMIT_STORAGE_URI", "memory://")
     RATELIMIT_STRATEGY = 'moving-window' # enforce default rate limit time window of 1 minute.
     
-    # admin email mailing list lol
+    # admin email stuff
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
