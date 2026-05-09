d0a39991ed282b73c427c6b9e968419e904635b8
Shivangi Sritharan <ssrith@proton.me>
2026-05-09 18:39:38 +0530

    email bug

--- app/core/email.py
+++ app/core/email.py
@@ -12,15 +12,16 @@ from flask import render_template, current_app
 from flask_mail import Message
 from extensions import mail
     
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
+#def send_async_email(app, msg):
+#    with app.app_context():
+#        mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_async_email, args=(current_app._get_current_object(), msg)).start()
+
+    mail.send(msg)
 
 # The mail.send() method needs to access the configuration values 
 # for the email server, and that can only be done by knowing what 
