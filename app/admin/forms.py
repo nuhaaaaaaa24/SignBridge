49a1c3df45e57b346309125279f626f400ec3cb8
Dulneth-La <dulnethlahiru320@gmail.com>
2026-05-14 10:58:21 +0530

    refined in-line comments admin forms

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -8,14 +8,14 @@ admin-related forms. The forms are
 implemented with Flask-WTForms.
 '''
 
-from flask_wtf import FlaskForm
-from wtforms import SubmitField
+from flask_wtf import FlaskForm # to get automatic CSRF protection for our forms
+from wtforms import SubmitField # import submit button field to generate the clickable buttons
 
-class ToggleAdminForm(FlaskForm):
-    submit = SubmitField('Toggle Admin')
+class ToggleAdminForm(FlaskForm): # form used by admins to toggle the admin status or remove admin privileges
+    submit = SubmitField('Toggle Admin') #
 
-class DeleteUserForm(FlaskForm):
+class DeleteUserForm(FlaskForm): # form used by admins to delete a user from the system
     submit = SubmitField('Delete')
 
-class UnblockUserForm(FlaskForm):
+class UnblockUserForm(FlaskForm): # form used by admins to unblock a user that was previously blocked
     submit = SubmitField('Unblock User')
