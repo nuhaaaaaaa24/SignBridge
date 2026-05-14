18cba42514207df1aae2ae2191a8a60cda5f6cf5
Dulneth-La <dulnethlahiru320@gmail.com>
2026-05-14 10:51:52 +0530

    refined in-line comments

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,5 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint # import Blueprint from the flask library to create a seperate module for admin routes
 
-admin_bp = Blueprint('admin', __name__)
+admin_bp = Blueprint('admin', __name__) # create the admin blueprint named 'admin' and the current module's name
 
-from app.admin import routes
+from app.admin import routes # import the admin routes so they are registered with this bp
